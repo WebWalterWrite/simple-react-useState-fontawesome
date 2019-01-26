8f25125eea33a404258642c6aa3ce3fb71038123
Sammy <32126283+WebWalterWrite@users.noreply.github.com>
2019-01-26 23:19:50 +0100

    git add
    fn setRank : recupére index du tableau et attribue le nombre d'étoiles relatif à l'index. Si la note est attribuée plus de 1fois le useState n'est pas mis à jour.
    
    fn resRank = réinitialise la note, si la note est déjà à 0, le useState ne peut pas être  reset à nouveau.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,58 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, memo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import icons
 import { faStar } from "@fortawesome/free-regular-svg-icons/";
+import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons/faStar";
 
 export default () => {
-  const [stars, setStars] = useState({ fullStars: 0, unStars: 5 });
+  let arrStars = Array(5).fill(faStar);
+
+  const [stars, setStars] = useState({ arr: arrStars, note: 0 });
+
+  /*
+   Récupérer le numéro d'index de l'élément et remplir le array à selon le l'index.
+*/
+  const setRank = e => {
+    let num = // récupérer l'index
+      parseInt(e.target.dataset.pos, 0) || // svg élément
+      parseInt(e.target.parentElement.dataset.pos, 0); // path élément;
+
+    num !== stars.note &&
+    arrStars.fill(fullStar, 0, num) && // Remplacer les étoiles vides par les étoiles pleines.
+      setStars({ arr: arrStars, note: num }); // modifier le state stars.
+  };
+
+  /*
+  Reset des notes
+*/
+  const resRank = () => {
+    stars.note !== 0 &&
+      arrStars.fill(faStar, 5) &&
+      setStars({ arr: arrStars, note: 0 });
+  };
 
   return (
-    <section>
-      <h2>Give a note for React</h2>
-      {[...Array(stars.unStars)].map(i => (
-        <FontAwesomeIcon key={i} icon={faStar} size="2x" />
-      ))}
-    </section>
+    <Fragment>
+      <section>
+        <h2>Give a note for React</h2>
+
+        {stars.arr.map((el, i) => (
+          <FontAwesomeIcon
+            key={i + 1}
+            icon={el}
+            size="2x"
+            data-pos={i + 1}
+            onClick={setRank}
+          />
+        ))}
+        <div>{stars.note}/5</div>
+      </section>
+
+      <section>
+        <div className="raz_stars">
+          <button onClick={resRank}>RAZ</button>
+        </div>
+      </section>
+    </Fragment>
   );
 };
